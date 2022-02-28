d334a6f4f8d5c773b4971f85c7a4f60748287894
DanTheMan314 <dankoshyg@gmail.com>
2022-02-28 23:13:21 +0530

    stashing

--- start.java
+++ start.java
@@ -14,14 +14,34 @@ class Start
                     "jdbc:oracle:thin:@localhost:1521:orcl2", "scott", "tigress");
 
             // step3 create the statement object
-            Statement stmt = con.createStatement();
+            
 
             // step4 execute query
-            ResultSet rs = stmt.executeQuery("select * from emp");
-            while (rs.next())
-                System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3));
-
-            // step5 close the connection object
+            Statement stmt = con.createStatement();
+            PreparedStatement ps = null;
+            // step4 execute query
+            ResultSet Stockings = stmt.executeQuery("create table stockings( pid number primary key"
+            +", product varchar(20), price number, qty number)");
+            ResultSet Orders = stmt.executeQuery("CREATE TABLE ORDERS (Oid number PRIMARY KEY, Prodid"
+            + " NUMBER,FOREIGN KEY (Prodid) REFERENCES Stockings(Pid), qty number, dateofclosing date)");
+            String s = "insert into stockings values(121,'cold coffee',88,10)";
+            ps = con.prepareStatement(s);
+            ps.execute();
+            s = "insert into stockings values(122,'doughnuts',120,20)";
+            ps = con.prepareStatement(s);
+            ps.execute();
+            //stmt.executeQuery("insert into orders values(1,121,5,to_date('28-02-2022','DD-MM-YYYY'))");
+            //stmt.executeQuery("insert into orders values(2,122,13,to_date('28-02-2022','DD-MM-YYYY'))");
+            ResultSet rsSt = stmt.executeQuery("select * from stockings");
+            //ResultSet rsOr = stmt.executeQuery("select * from orders");
+            while (rsSt.next())
+            {System.out.println(rsSt.getInt(1) + "  " + rsSt.getString(2) + "  \tRs." + rsSt.getInt(3)+" \tx "+rsSt.getInt(4));}
+            s = "drop table orders";
+            ps = con.prepareStatement(s);
+            boolean result = ps.execute();
+            s = "drop table stockings";
+            ps = con.prepareStatement(s);
+            result = ps.execute();
             con.close();
 
         } 
