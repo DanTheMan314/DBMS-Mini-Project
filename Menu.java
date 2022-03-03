668db24b470a4a7a5f7ebcc2d4323bcfc2736dd9
anugrahaantoo <anugrahaantoo@gmail.com>
2022-03-03 10:42:30 +0530

    list array

--- Menu.java
+++ Menu.java
@@ -2,8 +2,14 @@ import java.util.Scanner;
 import java.sql.*;
 
 public class Menu {
+    public class Product {
+        int pid, qty;
+    }
+
     public static void main(String args[]) {
         try {
+            Product list[] = new Product[10];
+            int i;
             // step1 load the driver class
             Class.forName("oracle.jdbc.driver.OracleDriver");
 
@@ -17,7 +23,7 @@ public class Menu {
 
             Scanner sc = new Scanner(System.in);
             String name;
-            int pid, Qty,orderno = 0;
+            int pid, Qty, orderno = 0;
             char ch = 'y';
             float price = 0, Tot_Price = 0;
             do {
@@ -31,7 +37,7 @@ public class Menu {
                     switch (c2) {
                         case 1:
                             System.out.println("Enter the product details:\nProduct name: ");
-                            
+
                             name = sc.nextLine();
                             System.out.println("Product id: ");
                             pid = sc.nextInt();
@@ -39,59 +45,60 @@ public class Menu {
                             price = sc.nextFloat();
                             System.out.println("Quantity: ");
                             Qty = sc.nextInt();
-                            String s = "insert into stockings values("+pid+",'"+name+"',"+price+","+Qty+")";
+                            String s = "insert into stockings values(" + pid + ",'" + name + "'," + price + "," + Qty
+                                    + ")";
                             ps = con.prepareStatement(s);
                             ps.execute();
                             break;
                         case 2:
-                        System.out.println("Product list:\n");
-                        s = "select product, price, qty FROM Stockings";
-                        ResultSet rs = stmt.executeQuery(s);
-                        System.out.println("Product name\tPrice\tQuantity\n");
-                        while(rs.next())
-                            System.out.println(rs.getString(1)+"\t"+rs.getFloat(2)+"\t"+rs.getInt(3));
+                            System.out.println("Product list:\n");
+                            s = "select product, price, qty FROM Stockings";
+                            ResultSet rs = stmt.executeQuery(s);
+                            System.out.println("Product name\tPrice\tQuantity\n");
+                            while (rs.next())
+                                System.out.println(rs.getString(1) + "\t" + rs.getFloat(2) + "\t" + rs.getInt(3));
                             System.out.println("Enter the name and id of the product to be updated: ");
                             name = sc.nextLine();
                             pid = sc.nextInt();
                             System.out.println("Quantity: ");
                             Qty = sc.nextInt();
-                            s = "update stockings set qty = "+Qty+" WHERE pid = "+pid;
+                            s = "update stockings set qty = " + Qty + " WHERE pid = " + pid;
                             rs = stmt.executeQuery(s);
                             break;
-                            case 3:
+                        case 3:
                             System.out.println("Product list:\n");
                             s = "select product, price, qty FROM Stockings";
                             rs = stmt.executeQuery(s);
                             System.out.println("Product name\tPrice\tQuantity\n");
-                            while(rs.next())
-                                System.out.println(rs.getString(1)+"\t"+rs.getFloat(2)+"\t"+rs.getInt(3));
+                            while (rs.next())
+                                System.out.println(rs.getString(1) + "\t" + rs.getFloat(2) + "\t" + rs.getInt(3));
                             System.out.println("Enter the id of the product on sale: ");
                             pid = sc.nextInt();
                             System.out.println("Enter Discount: ");
                             int disc = sc.nextInt();
-                            s = "select price from Stockings where product = "+pid;
+                            s = "select price from Stockings where product = " + pid;
                             rs = stmt.executeQuery(s);
                             rs.next();
                             price = rs.getFloat(1);
                             price += price * (disc / 100);
-                            s = "update stockings set price = "+price+" WHERE pid = "+pid;
+                            s = "update stockings set price = " + price + " WHERE pid = " + pid;
                             rs = stmt.executeQuery(s);
                             break;
-                            case 4:
+                        case 4:
                             System.out.println("Enter product name and id to be deleted: ");
                             name = sc.nextLine();
                             pid = sc.nextInt();
                             System.out.println("Product deleted!");
-                            s = "delete from stockings where pid = "+pid;
+                            s = "delete from stockings where pid = " + pid;
                             rs = stmt.executeQuery(s);
                             break;
-                            case 5:
+                        case 5:
                             System.out.println("Enter the name and id of the product: ");
                             name = sc.nextLine();
                             pid = sc.nextInt();
                             System.out.println("OrderID\tQuantity\n");
                             s = "select oid,qty from stockings s, orders o WHERE s.pid = o.prodid AND s.pid = "
-                            +pid+" AND dateofclosing >= SYSDATE";
+                                    + pid + " AND dateofclosing >= SYSDATE";
                             rs = stmt.executeQuery(s);
                             // SELECT OrderID, Qty AS Quantity Ordered FROM Stocking s, Orders o WHERE
                             // s.ProductID == o.pid AND s.ProductID == pid AND Date_Of_Closing >= SYSDATE;
@@ -104,20 +111,31 @@ public class Menu {
                     String s = "select product, price, qty FROM Stockings";
                     ResultSet rs = stmt.executeQuery(s);
                     System.out.println("Product name\tPrice\tQuantity\n");
-                    while(rs.next())
-                        System.out.println(rs.getString(1)+"\t"+rs.getFloat(2)+"\t"+rs.getInt(3));
-                    
+                    while (rs.next())
+                        System.out.println(rs.getString(1) + "\t" + rs.getFloat(2) + "\t" + rs.getInt(3));
+
+                    for (i = 0; i < 10; i++)
+                        list[i].pid = list[i].qty = 0;
+                    i = 0;
                     do {
                         System.out.println("Enter the name of the product: ");
                         name = sc.nextLine();
                         name = sc.nextLine();
                         System.out.println("Quantity to be bought: ");
                         Qty = sc.nextInt();
-                        s = "select qty,price,pid from Stockings where product = '"+name+"'";
+                        s = "select qty,price,pid from Stockings where product = '" + name + "'";
                         rs = stmt.executeQuery(s);
                         rs.next();
                         int Quantity = rs.getInt(1);
-                        if(Qty>Quantity)
+                        pid = rs.getInt(3);
+                        list[i].pid = pid;
+                        list[i].qty = Qty;
+                        i++;
+                        if (i > 9) {
+                            System.out.println("Exceeded cloud storage");
+                            break;
+                        }
+                        if (Qty > Quantity)
                             System.out.println("Exceeded quantity in stock!");
                         System.out.println("Do you want to order more products: ");
                         ch = sc.next().charAt(0);
@@ -127,8 +145,9 @@ public class Menu {
                     } while (ch == 'y');
                     System.out.println("Would you like to check out? ");
                     ch = sc.next().charAt(0);
-                    if(ch == 'y'){
-                        s = "insert into orders values("+orderno+","+rs.getInt(3)+","+Qty+
+
+                    if (ch == 'y') {
+                        s = "insert into orders values(" + orderno + "," + rs.getInt(3) + "," + Qty +
                                 ",add_months(sysdate,1))";
                         ps = con.prepareStatement(s);
                         ps.execute();
